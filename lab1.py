a3a2917a544cbd37f3a688bc15d3637758c8341f
vinay23fe10cse00607 <vinaynarwal50@gmail.com>
2024-11-26 21:45:40 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,19 @@
+""" Q1 Print the following
+
+a) print your first name
+
+b) print your last name
+
+c) print your complete name"""
+# (a)
+firstname = input("Enter your First name: ")
+print(firstname)
+#Output-Vansh
+# (b) 
+lastname = input("Enter your Last name: ")
+print(lastname)
+#Output - Tandon
+# (c)
+fullname = f"{firstname} {lastname}"
+print(fullname)
+#Output - Vansh Tandon
